66e375caae2030c5b1019a6dac462afae06c8190
LuisTavaresJr <ltsj13@gmail.com>
2020-01-06 15:27:34 -0700

    criação do register.html config. da view register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
-
+from .views import register
 
 urlpatterns = [
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-
+    path('cadastre-se/', register, name='register')
 ]
