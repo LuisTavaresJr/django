e83d249eb8bde6e714fd0b64ebbd56d58b99fd3e
LuisTavaresJr <ltsj13@gmail.com>
2020-01-06 23:53:00 -0700

    criação da def edit e html, block breadcrumb e dashboard_content

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register, dashboard
+from .views import register, dashboard, edit
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('sair/', LogoutView.as_view(next_page='core:home'), name='logout'),
-    path('cadastre-se/', register, name='register')
+    path('cadastre-se/', register, name='register'),
+    path('editar/', edit, name='edit')
 ]
