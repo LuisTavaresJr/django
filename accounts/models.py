dba9c2a7b232bd160bf3e8274554db01d16556b5
LuisTavaresJr <ltsj13@gmail.com>
2020-01-07 16:34:56 -0700

    criação do model passwordreset e utils.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,10 @@ import re
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.conf import settings
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-
 
     username = models.CharField('Nome de Usuário', max_length=30, unique=True, validators=[validators.RegexValidator(re.compile('^[\w.@+-]+'),
                                                                       'O nome do usuário so pode conter letras,'
@@ -37,3 +37,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
+
+
+class PasswordReset(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário', on_delete=models.CASCADE)
+    key = models.CharField('Chave', max_length=100, unique=True)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    confirmed = models.BooleanField('Confirmado?', default=False, blank=True) # pra saber por ex se esse link criado e enviado para o usuário ja foi usado
+
+    def __str__(self):
+        return f'{self.user} em {self.created_at}'
+
+    class Meta:
+        verbose_name = 'Nova Senha'
+        verbose_name_plural = 'Novas Senhas'
+        ordering = ['-created_at'] # ordenar em orde decrescente pela data criada
